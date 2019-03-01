a05c3f77bb60a6d4b0dc5b7512928e33d9b0ef8a
wamisnet <wamiwami@live.jp>
2019-03-01 22:19:43 +0900

    バッチファイル更新

---
+++ batOrigin/batClone.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /home/countit/CountIoT
+cp -r batOrigin/ bat/
+chmod 755 bat/*
--- batOrigin/usbsetting.sh
+++ batOrigin/usbsetting.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo echo KERNEL=="ttyUSB*",  ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="ttyUSB_TWELite" >> /etc/udev/rules.d/99-local.rules
+sudo echo KERNEL=="ttyUSB*",  ATTRS{interface}=="MONOSTICK", SYMLINK+="ttyUSB_TWELite" >> /etc/udev/rules.d/99-local.rules
